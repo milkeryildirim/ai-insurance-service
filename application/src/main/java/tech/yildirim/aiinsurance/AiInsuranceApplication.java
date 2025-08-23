f8fd957d6ebc7a009599599cd14b38465e0e3429
M.Ilker Yildirim <ext.ilker.yildirim@netgo.de>
2025-08-23 23:44:43 +0200

    Initialize Spring Boot project with Maven Wrapper. Add parent and application modules, project dependencies, and configuration for OpenAPI generation.

---
+++ application/src/main/java/tech/yildirim/aiinsurance/AiInsuranceApplication.java
@@ -0,0 +1,13 @@
+package tech.yildirim.aiinsurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiInsuranceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AiInsuranceApplication.class, args);
+  }
+
+}
